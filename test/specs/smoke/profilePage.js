a4ba15ead0fc21275019024fc6c872a655889b87
Andrii-KV <andrii.kovalev1@gmail.com>
2020-11-20 22:03:13 -0800

    test

--- test/specs/smoke/profilePage.js
+++ test/specs/smoke/profilePage.js
@@ -3,7 +3,6 @@ import ProfilePage from "../../pageobjects/profilePage";
 import user from "../../../data/users";
 
 describe("VERIFY THAT PROFILE PAGE HAS DIARY ELEMENTS", () => {
-
   // before(() => {
   //   LoginPage.open();
   //   //browser.url('https://stage.localcoding.us/user/login')
@@ -12,16 +11,14 @@ describe("VERIFY THAT PROFILE PAGE HAS DIARY ELEMENTS", () => {
   // });
 
   describe("VERIFY THAT PROFILE PAGE HAS DIARY ELEMENTS FOR ADMIN", () => {
-
-    before(()=> {
+    before(() => {
       LoginPage.open();
       LoginPage.login(user.admin.email, user.admin.password);
-    })
+    });
 
     it("Verify that diary tab exist in the top menu", () => {
       browser.pause(2000);
       expect(ProfilePage.topMenuDiary.isDisplayed()).toEqual(true);
-
     });
   });
 });
